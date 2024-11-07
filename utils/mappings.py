83d125fc1050ad8136495df6cec555ece6b21625
David Iguta <davidiguta@gmail.com>
2024-11-07 13:02:08 -0500

    data analysis

---
+++ utils/mappings.py
@@ -0,0 +1,22 @@
+# utils/course_mappings.py
+
+def get_course_mapping():
+    return {
+        33: "Biofuel Prod",
+        171: "Animation Design",
+        8014: "Social Service EC",
+        9003: "Agronomy",
+        9070: "Communication Design",
+        9085: "Veterinary Nursing",
+        9119: "Informatics Engineering",
+        9130: "Equinculture",
+        9147: "Management",
+        9238: "Social Service",
+        9254: "Tourism",
+        9500: "Nursing",
+        9556: "Oral Hygiene",
+        9670: "Advertising",
+        9773: "Journalism",
+        9853: "Education",
+        9991: "Management EC"
+    }
